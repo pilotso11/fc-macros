c14f780a81d0fa55b4d7408f8a54cfcbaba708b8
pilotso11 <seth@oshers.com>
2022-02-05 21:19:10 +0000

    Multi-image search for different HUD sizes

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # fc-macros
 Elite Dangerous Fleet Carrier Macros
 
-Currently this plugin uses default keybindings.   It may not work with custom HUD colors because it uses image
+Currently, this plugin uses default keybindings.   It may not work with custom HUD colors because it uses image
 matching to verify its screen navigation.
+Because the HUD sizes are different on some ships, this may cause issues.
+It's been tested on:
+* Imperial Cutter
+* Anaconda
+* Python
+* Type 9
+* Beluga
+* Dolphin
 
 Requires the following python addins:
 
--- main.py
+++ main.py
@@ -15,6 +15,17 @@ cool_down = False
 auto_jump = False
 route = []
 next_waypoint = ''
+route_file = ""
+is_odyssey = False
+
+carrier_services_set = ['images/carrier_services.png',   # cutter
+                        'images/carrier_services2.png',  # dolphin
+                        'images/carrier_services3.png',  # type 9
+                        'images/carrier_services4.png',  # mamba
+                        'images/carrier_services5.png',  # conda
+                        'images/carrier_services6.png',  # beluga
+                        'images/carrier_services7.png',  # python
+                        ]
 
 
 # Find most recent E:D log file
@@ -49,7 +60,18 @@ def load_route(route_file):
 
 # Press down key for down_time and wait after for delay
 def press(key, delay=0.1, down_time=0.2):
-    print("Press: ", key)
+    if key == '\b':
+        print("Press: backspace")
+    elif key == '\t':
+        print("Press: tab")
+    elif key == '\r':
+        print("Press: return")
+    elif key == '\n':
+        print("Press: newline")
+    elif key == ' ':
+        print("Press: space")
+    else:
+        print("Press: ", key)
     kb.press(key)
     sleep(down_time)
     kb.release(key)
@@ -71,22 +93,88 @@ def press_and_find(key='a', image='images/tritium.png', max_tries=10, confidence
     return True
 
 
+def press_and_find_set(key='a', images=[], max_tries=10, confidence=0.75):
+    cnt = 0
+    while True:
+        for i in images:
+            if pyautogui.locateOnScreen(i, confidence=confidence) is not None:
+                return True
+        press(key)
+        cnt += 1
+        if cnt > max_tries:
+            set_status('Macro failed')
+            return False
+
+
+def mouse_click_at(x, y, pause=0.25, click_duration=0.25):
+    pyautogui.moveTo(x, y)
+    sleep(pause)
+    pyautogui.mouseDown()
+    sleep(click_duration)
+    pyautogui.mouseUp()
+
+
+def schedule_jump():
+    if len(next_waypoint) < 3 or not auto_jump: return
+    use_mouse = False
+
+    set_status('Refueling')
+
+    if do_refuel.get() == 1:
+        refuel()
+
+    set_status('Finding next waypoint')
+
+    # Back to main menu
+    if not press_and_find_set('\b', carrier_services_set): return
+    press('space')
+    if not press_and_find('s', 'images/tritium_depot.png'): return
+    if not press_and_find('w', 'images/carrier_management.png'): return
+    press('space')
+    sleep(2)
+    if not press_and_find('s', 'images/navigation.png'): return
+    press('space')
+    sleep(0.5)
+    if not press_and_find('s', 'images/open_galmap.png'): return
+    press('space')
+    sleep(5)
+
+    if use_mouse:
+        pos = pyautogui.locateOnScreen('images/search_the_galaxy.png', confidence=0.75)
+        if pos is None:
+            set_status("Unable to find search")
+            return
+        x, y = pos.left + pos.width // 2, pos.top + pos.height // 2
+        mouse_click_at(x, y)
+        kb.write(next_waypoint)
+    else:
+        press('up')
+        kb.write(next_waypoint)
+        press('down')
+        press('space')
+
+    sleep(5)
+    if not press_and_find('space', 'images/set_carrier_destination.png'): return
+    print('Ready to set jump')
+
+
 def refuel():
     sleep(1)
-    load_tritium()
-    donate_tritium()
-    load_tritium()  # keep fully loaded to reduce tritium consumption
+    if not load_tritium(): return
+    if not donate_tritium(): return
+    if not load_tritium(): return  # keep fully loaded to reduce tritium consumption
 
 
 def load_tritium():
-    if not press_and_find('\b', 'images/carrier_services.png'): return
+    set_status('Filling ship with tritium')
+    if not press_and_find_set('\b', carrier_services_set): return False
     press('4', 0.5)
-    if not press_and_find('e', 'images/inventory.png'): return
+    if not press_and_find('e', 'images/inventory.png'): return False
     press('d')
     press('w')
-    if not press_and_find('d', 'images/transfer.png'): return
+    if not press_and_find('d', 'images/transfer.png'): return False
     press('space')
-    if not press_and_find('w', 'images/tritium.png'): return
+    if not press_and_find('w', 'images/tritium.png'): return False
 
     # hold down the 'a' key until max capacity is reached
     kb.press('a')
@@ -94,30 +182,32 @@ def load_tritium():
         sleep(0.5)
     kb.release('a')
 
-    if not press_and_find('s', 'images/cancel.png'): return
+    if not press_and_find('s', 'images/cancel.png'): return False
     press('d')
     press('space')
     set_status('tritium loaded')
-    if not press_and_find('\b', 'images/carrier_services.png'): return
+    if not press_and_find_set('\b', carrier_services_set): return False
+
+    return True
 
 
 def donate_tritium():
-    if not press_and_find('\b', 'images/carrier_services.png'): return
+    set_status('Donating tritium')
+    if not press_and_find_set('\b', carrier_services_set): return False
     press('space')
-    if not press_and_find('s', 'images/tritium_depot.png'): return
+    if not press_and_find('s', 'images/tritium_depot.png'): return False
     press('space')
     sleep(0.5)
-    if not press_and_find('w', 'images/donate_tritium.png'): return
+    if not press_and_find('w', 'images/donate_tritium.png'): return False
     press('space')
-    if not press_and_find('w', 'images/confirm_deposit.png'): return
+    if not press_and_find('w', 'images/confirm_deposit.png'): return False
     press('space')
     set_status('tritium donated')
-    if not press_and_find('s', 'images/exit_door.png'): return
+    if not press_and_find('s', 'images/exit_door.png'): return False
     press('space')
-    if not press_and_find('\b', 'images/carrier_services.png'): return
+    if not press_and_find_set('\b', carrier_services_set): return False
 
-
-route_file = ""
+    return True
 
 
 def select_route_file():
@@ -128,12 +218,18 @@ def select_route_file():
 
 
 def update_route_position():
-    global next_waypoint
+    global next_waypoint, jumping
+    if len(route) == 0: return
+
+    next_waypoint = ""  # Next waypoint requires positive validation
+
     route_len_label.config(text="{} via {} waypoints".format(route[-1], len(route)))
     n = 0
     if current_system == route[-1]:
         route_pos_label.config(text="Destination reached")
+        jumping = False
         return
+
     for r in route:
         if r == current_system:
             next_waypoint = route[n+1]
@@ -142,6 +238,8 @@ def update_route_position():
         n += 1
     route_pos_label.config(text="First waypoint is {}".format(route[0]))
 
+
+# Setup UI
 root = Tk()
 
 do_refuel = tkinter.IntVar()
@@ -206,10 +304,12 @@ def cool_down_complete():
     cool_down = False
     print("Carrier cool down compete")
     set_status("Carrier cool down complete")
+    if jumping:
+        schedule_jump()
 
 
 def process_log():
-    global current_system, jumping, cool_down
+    global current_system, jumping, cool_down, is_odyssey
     lines = ed_log.readlines()
     # print("Got ", len(lines), "lines from E:D log")
     curr_sys = ''
@@ -218,6 +318,16 @@ def process_log():
         print(line)
         if line.count('StarSystem') > 0:
             curr_sys = line
+        if line.count('Odyssey') > 0:
+            parts = line.split(',')
+            for p in parts:
+                keys = p.split(':')
+                k = keys[0].strip('" ')
+                if k == 'Odyssey':
+                    ody = keys[1]
+                    if ody == "true":
+                        is_odyssey = True
+                        print("We're in Odyssey")
 
     if curr_sys > '':
         new_system = curr_sys.split('"StarSystem":"')[1].split('"')[0]
@@ -229,6 +339,7 @@ def process_log():
                 root.after(1000 * 180, cool_down_complete)
             print("Current system=", current_system)
             system_label.config(text=current_system)
+            update_route_position()
             root.update_idletasks()
 
     root.after(1000, process_log)
@@ -240,9 +351,17 @@ def set_status(msg=''):
 
 
 def check_keys():
+    global auto_jump
     if kb.is_pressed('ctrl+f11'):
         set_status("ctrl+f11 pressed")
         root.after(100, refuel())
+    if kb.is_pressed('ctrl+f12'):
+        set_status("ctrl+f12 pressed")
+        auto_jump = True
+        root.after(100, schedule_jump())
+    if kb.is_pressed('ctrl+f10'):
+        set_status("ctrl+f10 pressed")
+        auto_jump = False
     root.after(20, check_keys)
 
 
