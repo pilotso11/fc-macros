70205cac4ece71ded6d89bee1c427b3e3309e3e5
pilotso11 <seth@oshers.com>
2022-02-05 16:56:24 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,97 @@
+from tkinter import *
+from tkinter import ttk
+import keyboard as kb
+import pyautogui
+from time import sleep
+
+# Press down key for down_time and wait after for delay
+def press(key, delay=0.1, down_time=0.2):
+    print("Press: ", key)
+    kb.press(key)
+    sleep(down_time)
+    kb.release(key)
+    sleep(delay)
+
+
+# look for the image on screen , if found, return True
+# if not, press the key specified, true this up to max_trues times,
+# if max_tries is exceeded return False
+# confidence is the confidence interval on the match
+def press_and_find(key='a', image='images/tritium.png', max_tries=10, confidence=0.75):
+    cnt = 0
+    while pyautogui.locateOnScreen(image, confidence=confidence) is None:
+        press(key)
+        cnt += 1
+        if cnt > max_tries:
+            set_status('Macro failed')
+            return False
+    return True
+
+
+def refuel():
+    n = 3
+    while n > 0:
+        print("t-{}".format(n))
+        sleep(1)
+        n -= 1
+    load_tritium()
+    donate_tritium()
+    load_tritium()   # keep fully loaded to reduce tritium consumption
+
+def load_tritium():
+    if not press_and_find('\b', 'images/carrier_services.png'): return
+    press('4', 0.5)
+    if not press_and_find('e', 'images/inventory.png'): return
+    press('d')
+    press('w')
+    if not press_and_find('d', 'images/transfer.png'): return
+    press('space')
+    if not press_and_find('w', 'images/tritium.png'): return
+
+    # hold down the 'a' key until max capacity is reached
+    kb.press('a')
+    while pyautogui.locateOnScreen('images/max_capacity.png', confidence=0.75) is None:
+        sleep(0.5)
+    kb.release('a')
+
+    if not press_and_find('s', 'images/cancel.png'): return
+    press('d')
+    press('space')
+    set_status('tritium loaded')
+    if not press_and_find('\b', 'images/carrier_services.png'): return
+
+def donate_tritium():
+    if not press_and_find('\b', 'images/carrier_services.png'): return
+    press('space')
+    if not press_and_find('s', 'images/tritium_depot.png'): return
+    sleep(0.5)
+    press('space')
+    if not press_and_find('w', 'images/confirm_deposit.png'): return
+    set_status('tritium donated')
+    if not press_and_find('\b', 'images/carrier_services.png'): return
+
+
+root = Tk()
+frame = ttk.Frame(root, padding=10)
+frame.grid()
+ttk.Label(frame, text="Fleet Carrier Tools").grid(column=0, row=0)
+ttk.Button(frame, text="Refuel Ctrl+F11", command=refuel).grid(column=0, row=1)
+status = ttk.Label(frame, text="this is the status bar --- nice and wide")
+status.grid(column=0, row=2)
+ttk.Button(frame, text="Quit", command=root.destroy).grid(column=0, row=3)
+
+
+def set_status(msg=''):
+    status.config(text="ctrl+f11 pressed")
+    root.update_idletasks()
+
+
+def check_keys():
+    if kb.is_pressed('ctrl+f11'):
+        set_status("ctrl+f11 pressed")
+        root.after(100, refuel())
+    root.after(20, check_keys)
+
+
+check_keys()
+root.mainloop()
